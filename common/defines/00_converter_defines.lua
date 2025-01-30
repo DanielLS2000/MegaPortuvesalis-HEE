7a14b385a163dc298407c3b2e5926160a7ff8c04
Daniel <daniellimas2000@gmail.com>
2025-01-30 12:18:08 -0300

    Russia monument & Livonia Missions

--- common/defines/00_converter_defines.lua
+++ common/defines/00_converter_defines.lua
@@ -1,2 +1,4 @@
 -- To be edited dynamically by the converter.
 NDefines.NDiplomacy.IMPERIAL_AUTHORITY_FROM_PRINCES = 0.025
+NDefines.NDiplomacy.HRE_FOREIGN_CONTROL_PENALTY = -0.002
+NDefines.NDiplomacy.HRE_HERETIC_PENALTY = -0.003
